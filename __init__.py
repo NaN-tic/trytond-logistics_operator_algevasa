898f60596efa653efcafda56452058a0b6085147
Bernat Brunet <bernat@nan-tic.com>
2025-02-02 17:44:26 +0100

    Add default product configuration to set "algevasa" check to True when create a product.
    Update the xml_parse to ensure that parse correctly the SOAP answer and
    the file that Algevasa send to Tryton webservice.
    
    Check the Algevasa XML and write the possible issues.
    
    Task: #173821

--- __init__.py
+++ __init__.py
@@ -3,15 +3,12 @@
 # the full copyright notices and license terms.
 from trytond.pool import Pool
 
-from . import location, stock, product
+from . import configuration, location, stock, product
 
 def register():
     Pool.register(
+        configuration.Configuration,
         location.Location,
         stock.ShipmentOut,
         product.Product,
         module='logistics_operator_algevasa', type_='model')
-    Pool.register(
-        module='logistics_operator_algevasa', type_='wizard')
-    Pool.register(
-        module='logistics_operator_algevasa', type_='report')
