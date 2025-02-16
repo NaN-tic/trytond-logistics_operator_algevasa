486e7a41177f11d539a0b0263cd4d33ea4669380
Bernat Brunet <bernat@nan-tic.com>
2025-02-16 22:04:55 +0100

    Add the possibility to send the shipment to Algevasa on done or on assign. If the shipment is send on assign, when the answer from Algevasa i received and all is OK, the shipment is done automatically.

--- location.py
+++ location.py
@@ -17,14 +17,26 @@ class Location(metaclass=PoolMeta):
         states={
             'invisible': ~Eval('algevasa', False),
             'required': Eval('algevasa', False),
-            })
+            },
+        help="The template file useded to synch products to Algevasa")
     algevasa_shipment_format = fields.Many2One('file.format',
         'Algevasa Shipment File Format',
         states={
             'invisible': ~Eval('algevasa', False),
             'required': Eval('algevasa', False),
-            })
+            },
+        help="The template file useded to synch shipments to Algevasa")
+    algevasa_on_done = fields.Boolean('Algevasa On Done',
+        states={
+            'invisible': Eval('type') != 'warehouse',
+            },
+        help="If checked the shipment is synched with Algevasa when 'Done' "
+        "the shipment. If unchecked the synched happens in the 'Assign'")
 
     @staticmethod
     def default_algevasa():
+        return False
+
+    @staticmethod
+    def default_algevasa_on_done():
         return False
